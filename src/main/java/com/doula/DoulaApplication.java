b227c92834e753736eb67d9592679a8248abe879
Tynan <quixotichamster@gmail.com>
2016-12-03 16:29:08 -0600

    Initializing Spring Maven files

---
+++ src/main/java/com/doula/DoulaApplication.java
@@ -0,0 +1,12 @@
+package com.doula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoulaApplication.class, args);
+	}
+}

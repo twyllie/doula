9138e9a665fe3a5b419b105e690363f5c073c614
Tynan <quixotichamster@gmail.com>
2017-03-06 16:58:57 -0600

    Solved another, on to the next

--- src/main/java/com/doula/WebApplicationConfig.java
+++ src/main/java/com/doula/WebApplicationConfig.java
@@ -1,21 +1,21 @@
-package com.doula;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Configuration
-public class WebApplicationConfig extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public AuthenticationInterceptor authenticationInterceptor() {
-        return new AuthenticationInterceptor();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor( authenticationInterceptor() );
-    }
-
-}
+//package com.doula;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//public class WebApplicationConfig extends WebMvcConfigurerAdapter {
+//
+//    @Bean
+//    public AuthenticationInterceptor authenticationInterceptor() {
+//        return new AuthenticationInterceptor();
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor( authenticationInterceptor() );
+//    }
+//
+//}
